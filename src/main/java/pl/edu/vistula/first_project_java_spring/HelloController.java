52cc4c6f4a735dd50d195750a83de3e13e4d8f99
sebastiangago <sebastiangago02@gmail.com>
2025-01-15 20:58:24 +0100

    commit

--- src/main/java/pl/edu/vistula/first_project_java_spring/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/HelloController.java
@@ -1,14 +1,18 @@
 package pl.edu.vistula.first_project_java_spring;
+
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {return "Hello Vistula, in my first Spring controller";}
+    public String hello() {
+        return "Hello Vistula, in my first Spring controller.";
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
